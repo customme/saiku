0a5e1861491b993dec4879180ad06d4a77ba3336
customme@163.com <customme@163.com>
2017-11-17 14:56:04 +0800

    编译成功

--- buildall.bat
+++ buildall.bat
@@ -6,20 +6,20 @@ cd ..
 echo "Building saiku-webapp"
 cd saiku-webapp
 call mvn clean install
-cd ..
-call git submodule init
-call git submodule update
+@rem cd ..
+@rem call git submodule init
+@rem call git submodule update
 echo "Building saiku-ui"
 cd ..\saiku-ui
-call git checkout master
-call git pull
+@rem call git checkout master
+@rem call git pull
 call mvn clean package install:install-file -Dfile=target/saiku-ui-2.6.1-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.6.1-SNAPSHOT -Dpackaging=war
 echo "Building saiku-server"
-cd ..\saiku\saiku-server
+cd ..\saiku-server
 call mvn clean package
-echo "Building saiku-bi-platform-plugin"
-cd ..\saiku-bi-platform-plugin
-call mvn clean package
-echo "Building saiku-bi-platform-plugin-p5"
-cd ..\saiku-bi-platform-plugin-p5
-call mvn clean package
+@rem echo "Building saiku-bi-platform-plugin"
+@rem cd ..\saiku-bi-platform-plugin
+@rem call mvn clean package
+@rem echo "Building saiku-bi-platform-plugin-p5"
+@rem cd ..\saiku-bi-platform-plugin-p5
+@rem call mvn clean package
---
+++ cleanall.bat
@@ -0,0 +1,9 @@
+
+cd saiku-core
+call mvn clean
+cd ../saiku-webapp
+call mvn clean
+cd ..\saiku-ui
+call mvn clean
+cd ..\saiku-server
+call mvn clean
